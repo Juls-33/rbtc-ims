a88083dd59ba18be1ca15d99f7a486c9864bda1b
Juls-33 <juliusaustin.santos@gmail.com>
2026-03-26 15:21:05 +0800

    logo update

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -2,6 +2,7 @@ export default function ApplicationLogo(props) {
     return (
         <svg
             {...props}
+            src="/public/assets/logo.png"
             viewBox="0 0 316 316"
             xmlns="http://www.w3.org/2000/svg"
         >
